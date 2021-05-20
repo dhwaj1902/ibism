816aceca68c7e95f34bf559265aa31fefaa48bb4
Dhwaj Gupta <57860123+dhwaj1902@users.noreply.github.com>
2021-05-20 23:43:42 +0530

    Remove key

--- src/config/Firebase.js
+++ src/config/Firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase";
 import "firebase/storage";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyCFqOYzDs7uqfzwMzJA9iTvyTdx36V9Ukk",
-  authDomain: "ibism-b8e2f.firebaseapp.com",
-  projectId: "ibism-b8e2f",
-  storageBucket: "ibism-b8e2f.appspot.com",
-  messagingSenderId: "307227939732",
-  appId: "1:307227939732:web:488999fbae789c5413b581",
-  measurementId: "G-J4KFP2XJQ5",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 firebase.initializeApp(firebaseConfig);
